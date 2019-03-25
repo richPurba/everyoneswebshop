091f1067879e1a8eab59c935df5074636d68f717
Richard Tymotheus Purba <tepetrol@yahoo.com>
2019-03-25 20:39:04 +0100

    providing infrastructure for front-end, including providing polymer for development

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path=require("path");
+
+module.exports={
+    entry:'src/main/js/index.js',
+    devTool:'sourcemaps',
+    cache:true,
+    mode:'development',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: path.join(__dirname,'.'),
+                exclude: /(node_modules)/,
+                use: [
+                    {loader:'babel-loader',
+                    options:{
+                        presets:["@babel/preset-env"]
+                    }
+
+                },
+                    {loader:'polymer-webpack-loader'}
+
+                    ]
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    }
+}
