08638cab9ac012294a9a69301c6668362cc7a4c4
RichardPurba <tepetrol@yahoo.com>
2019-03-25 10:36:25 +0100

    adding springboot application

---
+++ src/main/java/com/everyone/everyoneswebshop/EveryoneswebshopApplication.java
@@ -0,0 +1,13 @@
+package com.everyone.everyoneswebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryoneswebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryoneswebshopApplication.class, args);
+	}
+
+}
