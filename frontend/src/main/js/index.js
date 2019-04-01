926bdc2a914adde7ef48291a11f6114ed0a79a0d
RichardPurba <tepetrol@yahoo.com>
2019-04-01 20:48:12 +0200

    finally creating a framework for React

--- frontend/src/main/js/index.js
+++ frontend/src/main/js/index.js
@@ -4,10 +4,7 @@ const React = require('react');
 const ReactDom = require('react-dom');
 
 class App extends React.Component{
-    render(){
-        return(
-            )
-    }
+
 }
 
 ReactDom.render(<App/>,
